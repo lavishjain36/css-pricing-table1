b664320a62b1a9b60d43ed51b22c992b6956018b
Lavish Jain <lavishjain36@gmail.com>
2021-10-11 10:01:56 +0530

    dom practice

--- script.js
+++ script.js
@@ -1,141 +1,46 @@
-let employees = [
-  {
-    id: 1,
-    first_name: "Valaree",
-    last_name: "Chin",
-    email: "vchin0@diigo.com",
-    gender: "Female",
-    ip_address: "150.193.177.24",
-  },
-  {
-    id: 2,
-    first_name: "Olva",
-    last_name: "Ainsbury",
-    email: "oainsbury1@merriam-webster.com",
-    gender: "Female",
-    ip_address: "246.224.50.123",
-  },
-  {
-    id: 3,
-    first_name: "Gail",
-    last_name: "Evesque",
-    email: "gevesque2@smugmug.com",
-    gender: "Female",
-    ip_address: "233.14.29.117",
-  },
-  {
-    id: 4,
-    first_name: "Reeta",
-    last_name: "Hanstock",
-    email: "rhanstock3@archive.org",
-    gender: "Female",
-    ip_address: "83.47.171.190",
-  },
-  {
-    id: 5,
-    first_name: "Gearard",
-    last_name: "Smeeth",
-    email: "gsmeeth4@ycombinator.com",
-    gender: "Male",
-    ip_address: "128.91.65.8",
-  },
-  {
-    id: 6,
-    first_name: "Hal",
-    last_name: "Giacomello",
-    email: "hgiacomello5@intel.com",
-    gender: "Male",
-    ip_address: "43.247.218.29",
-  },
-  {
-    id: 7,
-    first_name: "Derby",
-    last_name: "Riddlesden",
-    email: "driddlesden6@linkedin.com",
-    gender: "Male",
-    ip_address: "147.51.104.85",
-  },
-  {
-    id: 8,
-    first_name: "Betti",
-    last_name: "Josselsohn",
-    email: "bjosselsohn7@reddit.com",
-    gender: "Female",
-    ip_address: "104.159.88.167",
-  },
-  {
-    id: 9,
-    first_name: "Christophorus",
-    last_name: "Stranks",
-    email: "cstranks8@dedecms.com",
-    gender: "Male",
-    ip_address: "47.248.114.51",
-  },
-  {
-    id: 10,
-    first_name: "Dosi",
-    last_name: "Boshers",
-    email: "dboshers9@tuttocitta.it",
-    gender: "Female",
-    ip_address: "248.166.167.7",
-  },
-];
+// on Change Event
 
-// All Employees
-let allEmpButton = document.querySelector("#all-emp-btn");
-allEmpButton.addEventListener("click", function () {
-  displayEmployees(employees);
+// get the element
+let selectBox = document.querySelector("#select-tag");
+// hook up an event listener
+selectBox.addEventListener("change", function () {
+  let selectedOption = selectBox.value;
+  let spanTag = document.querySelector("#span-1");
+  spanTag.innerText = selectedOption;
 });
 
-// Male Employees
-let maleEmpButton = document.querySelector("#male-emp-btn");
-maleEmpButton.addEventListener("click", function () {
-  let maleEmployees = employees.filter(function (employee) {
-    return employee.gender === "Male";
-  });
-  displayEmployees(maleEmployees);
-});
-
-// Female Employees
-let femaleEmpButton = document.querySelector("#female-emp-btn");
-femaleEmpButton.addEventListener("click", function () {
-  let femaleEmployees = employees.filter(function (employee) {
-    return employee.gender === "Female";
-  });
-  displayEmployees(femaleEmployees);
-});
-
-// Search Employees
-let searchBox = document.querySelector("#search-box");
-searchBox.addEventListener("keyup", function () {
-  let textEntered = searchBox.value;
-  let filteredEmployees = [];
-  if (textEntered !== "") {
-    filteredEmployees = employees.filter(function (employee) {
-      return employee.first_name
-        .toUpperCase()
-        .startsWith(textEntered.toUpperCase());
-    });
-    document.querySelector("#table-body").innerHTML = "";
-    displayEmployees(filteredEmployees);
-  } else {
-    document.querySelector("#table-body").innerHTML = "";
+// key up event
+let reverseString = (str) => {
+  let tempStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    tempStr += str.charAt(i);
   }
-});
-
-// display Employees
-let displayEmployees = (employees) => {
-  let tableBody = document.querySelector("#table-body");
-  let tableRow = "";
-  for (let employee of employees) {
-    tableRow += `<tr>
-                        <td>${employee.id}</td>
-                        <td>${employee.first_name}</td>
-                        <td>${employee.last_name}</td>
-                        <td>${employee.email}</td>
-                        <td>${employee.gender}</td>
-                        <td>${employee.ip_address}</td>
-                     </tr> \n`;
-    tableBody.innerHTML = tableRow;
-  }
+  return tempStr;
 };
+let textBox = document.querySelector("#username");
+textBox.addEventListener("keyup", function () {
+  let textEntered = textBox.value;
+  let h3Tag = document.querySelector("#user-text");
+  h3Tag.innerText = reverseString(textEntered);
+});
+
+// SMS App
+let textArea = document.querySelector("#text-area");
+let charCount = 100;
+textArea.addEventListener("keyup", function () {
+  let textEnteredLength = textArea.value.length;
+  let spanTag = document.querySelector("#span-2");
+  spanTag.innerText = charCount - textEnteredLength;
+});
+
+// Change the Password
+let checkBox = document.querySelector("#check");
+checkBox.addEventListener("change", function () {
+  let passwordBox = document.querySelector("#password");
+  let theAttribute = passwordBox.getAttribute("type");
+  if (theAttribute === "password") {
+    passwordBox.setAttribute("type", "text");
+  } else {
+    passwordBox.setAttribute("type", "password");
+  }
+});
